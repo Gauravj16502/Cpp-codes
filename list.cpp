31c21964d5a73b2ee524dad78307cf3fb0213364
Human_spider <gjain16502@gmail.com>
2019-10-15 22:39:39 +0530

    removed all comments

--- list.cpp
+++ list.cpp
@@ -5,18 +5,14 @@ int main()
 {
     list <int> LI;
     list <int>::iterator it;
-    //inserts elements at end of list
     LI.push_back(4);
     LI.push_back(5);
 
-    //inserts elements at beginning of list
     LI.push_front(3);
     LI.push_front(5);
 
-    //returns reference to first element of list
     it = LI.begin();
 
-    //inserts 1 before first element of list
     LI.insert(it,1);
 
     cout<<"All elements of List LI are: " <<endl;
@@ -26,7 +22,6 @@ int main()
     }
     cout<<endl;
 
-    //reverse elements of list
     LI.reverse();
 
     cout<<"All elements of List LI are after reversing: " <<endl;
@@ -36,7 +31,6 @@ int main()
     }
     cout<<endl;
 
-    //removes all occurences of 5 from list
     LI.remove(5);
 
     cout<<"Elements after removing all occurence of 5 from List"<<endl;
@@ -46,9 +40,7 @@ int main()
     }
     cout<<endl;
 
-    //removes last element from list
     LI.pop_back();
-    //removes first element from list
     LI.pop_front();
     return 0;
 }
